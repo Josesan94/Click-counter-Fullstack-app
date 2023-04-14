df77c950783122c2025bce889d601cdfa79c05c8
Josesan94 <josesantillan2@hotmai>
2023-04-14 17:21:53 -0300

    enable CORS

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 
 const express = require('express')
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const buttonRoutes = require('./src/routes/buttonRoutes');
 const app = express()
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
